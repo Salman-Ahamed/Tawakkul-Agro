3aa963201c2ba2e42bd46a849be9ad6021ebe795
Salman-Ahamed <me.shahriyarhosen@gmail.com>
2025-09-26 10:11:20 +0600

    Add Bengali font support and custom styles to globals.css

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,3 +2,5 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 export const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 export const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
+
+// For Bengali support, we'll use CSS import in globals.css
